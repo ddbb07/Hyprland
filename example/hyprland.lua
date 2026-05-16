4162803e425b27cc54a37d13c3f05f998ab61e7d
ddbb07 <50288525+ddbb07@users.noreply.github.com>
2026-05-16 14:00:45 +0200

    Add environment variables

--- example/hyprland.lua
+++ example/hyprland.lua
@@ -96,6 +96,31 @@ hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 
 
+-- Misc --
+hl.env("EDITOR", "nano")
+hl.env("BASH_SHARED_SCRIPT_PATH", "/home/davidb/Dokument/Kodning/Bash-Scripting/shared.sh")
+
+-- XDG --
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XKB_DEFAULT_LAYOUT", "se")
+
+-- NVidia --
+hl.env("LIBVA_DRIVER_NAME", "nvidia")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("GBM_BACKEND", "nvidia-drm")
+hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+hl.env("__GL_GSYNC_ALLOWED", "0")
+hl.env("__GL_VRR_ALLOWED", "0")
+
+-- Toolkit --
+hl.env("GDK_BACKEND", "wayland,x11,*")
+hl.env("QT_QPA_PLATFORM", "wayland;xcb")
+hl.env("SDL_VIDEODRIVER", "wayland")
+hl.env("CLUTTER_BACKEND", "wayland")
+hl.env("ELECTRON_OZONE_PLATFORM_HINT", "auto")
+
 -----------------------
 ----- PERMISSIONS -----
 -----------------------
